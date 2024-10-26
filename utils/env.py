2463ff24a6ca1f6aa9b4f596f8d0ca8d159b4c95
davel-go <joel.dav.gomez@gmail.com>
2024-10-26 01:30:10 -0600

    Added logging to env vars extraction

--- utils/env.py
+++ utils/env.py
@@ -1,16 +1,23 @@
 import os
 from dotenv import load_dotenv
+from log import log_action
 
-# Ruta al archivo .env en el directorio raíz del proyecto
+log_action("INITIALIZING ENV VARS EXTRACTION TASK ===== ===== ===== =====")
+
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ENV_PATH = os.path.join(ROOT_DIR, '.env')
 
-# Cargar las variables de entorno desde el archivo .env
 load_dotenv(ENV_PATH)
 
-# Ejemplo de acceso a las variables de entorno
 DB_NAME = os.getenv('DB_NAME', 'test')
 WORK_DIR = os.getenv('WORKING_DIR', '/')
 
-print("Database Name:", DB_NAME)
-print("Work Dir:", WORK_DIR)
+if not os.path.exists(WORK_DIR):
+    WORK_DIR = os.path.abspath(os.sep)
+    log_action(f"Directorio no encontrado. WORK_DIR se establece en el directorio root: {work_dir}", "WARNING")
+
+log_action(f"   Database Name: {DB_NAME} ")
+log_action(f"   Working Directory: {WORK_DIR} ")
+
+print("Database Name: ", DB_NAME)
+print("Work Dir: ", WORK_DIR)
