ea428ec973339e10f8b77f296cd4e6c8a64edd8a
davel-go <joel.dav.gomez@gmail.com>
2024-10-26 01:47:50 -0600

    Created function to start main table

---
+++ database/init.py
@@ -0,0 +1,25 @@
+import sqlite3
+from utils.log import log_action
+
+
+def initialize_events_table(conn: sqlite3.Connection):
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS EVENTS (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        story_arc TEXT NOT NULL,
+        type TEXT NOT NULL,
+        desc TEXT,
+        day INTEGER,
+        month INTEGER,
+        year INTEGER,
+        anchor_event INTEGER,
+        before BOOLEAN NOT NULL,
+        a_day INTEGER,
+        a_month INTEGER,
+        a_year INTEGER
+    )
+    ''')
+    # Save changes
+    conn.commit()
+    log_action("Events table initialized")
