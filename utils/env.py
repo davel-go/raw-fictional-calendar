380a02bf97e7c91e9c1759ea3fd3589e742472b1
davel-go <joel.dav.gomez@gmail.com>
2024-10-26 01:43:58 -0600

    Fixed language and var name issue

--- utils/env.py
+++ utils/env.py
@@ -14,7 +14,7 @@ WORK_DIR = os.getenv('WORKING_DIR', '/')
 
 if not os.path.exists(WORK_DIR):
     WORK_DIR = os.path.abspath(os.sep)
-    log_action(f"Directorio no encontrado. WORK_DIR se establece en el directorio root: {work_dir}", "WARNING")
+    log_action(f"Directory not found. WORK_DIR will be: {WORK_DIR}", "WARNING")
 
 log_action(f"   Database Name: {DB_NAME} ")
 log_action(f"   Working Directory: {WORK_DIR} ")
