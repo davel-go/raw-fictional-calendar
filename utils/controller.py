134b4a6d5f1ee56f440a552b283f9940a898aaf3
davel-go <joel.dav.gomez@gmail.com>
2024-10-30 23:30:24 -0600

    Added logging to controller get_event action

--- utils/controller.py
+++ utils/controller.py
@@ -1,10 +1,15 @@
 from database.conn import CONN
 from database.queries import db_get_event
 from entity.EventPreload import EventPreload
+from utils.log import log_action
 
 
 def get_event(id):
+    log_action(f"CONTROLLER ACTION  --get_event--   Searching event with ID: {id}")
     event = db_get_event(CONN, id)
-    if event is not None:
+    if event is None:
+        log_action(f"    Event not found (event id: {id})", "WARNING")
+    else:
         event = EventPreload(*event)
+        log_action(f"    Successful extraction of event {id}")
     return event
