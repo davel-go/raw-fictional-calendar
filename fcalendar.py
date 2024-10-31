b171c52467d28b02116abc4d732a19f03ac6ff16
davel-go <joel.dav.gomez@gmail.com>
2024-10-30 23:11:03 -0600

    Deleted comments

--- fcalendar.py
+++ fcalendar.py
@@ -1,13 +1,12 @@
 import argparse
 
+
 def main():
-    # Configura el parser de argumentos
     parser = argparse.ArgumentParser(
         prog="fcalendar",
         description="fcalendar: CLI to manage fictional calendar data"
     )
 
-    # Mensaje de ayuda personalizado para --help
     parser.add_argument(
         '-v', '--version',
         action='version',
@@ -15,11 +14,10 @@ def main():
         help="shows the app version"
     )
 
-    # Parsear los argumentos proporcionados por el usuario
     args = parser.parse_args()
 
-    # Puedes incluir el código que se ejecutará al llamar a fcalendar (opcional)
     print("Welcome to fcalendar. Use '--help' to see the available options.")
+
 
 if __name__ == "__main__":
     main()
